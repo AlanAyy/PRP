e01a10556c6e1b874d8dcd97703ac35595fbfde8
AlanAyy <58347931+AlanAyy@users.noreply.github.com>
2023-05-19 15:28:03 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ params = dict()
 
 params['num_classes'] = 101
 
-params['dataset'] = '/home/Dataset/UCF-101-origin'
+params['dataset'] = 'D:/Projects/ai-research-school/datasets/UCF101'
 #params['dataset'] = '/home/Dataset/hmdb'
 
-
-params['epoch_num'] = 150#600
-params['batch_size'] = 8
+params['epoch_num'] = 600  # 600
+params['batch_size'] = 2
+params['epoch_checkpoints'] = 20
 params['step'] = 10
 params['num_workers'] = 4
-params['learning_rate'] = 0.001
+params['learning_rate'] = 0.01  # 0.001
 params['momentum'] = 0.9
 params['weight_decay'] = 0.0005
 params['display'] = 100#10
@@ -18,7 +18,5 @@ params['pretrained'] = None
 params['gpu'] = [0]
 params['log'] = 'log'
 #params['save_path'] = 'UCF101'
-params['save_path_base']='/home/Workspace/PRP/outputs/'
+params['save_path_base']='D:/Projects/ai-research-school/PRP/outputs/'
 params['data']='UCF-101'
-
-
